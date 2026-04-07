6691770704d86e41d5d33480952904cbb0cade3e
rkalgreen <ryan.kalgreen@gmail.com>
2026-04-07 13:36:28 -0500

    changed i2c pins, added blink and print debug tests

--- HW3/i2c_intro/i2c_intro.c
+++ HW3/i2c_intro/i2c_intro.c
@@ -5,10 +5,11 @@
 
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
+// Sample code changed to I2C1 on GPIO14 (SDA) and GPIO15 (SCL)
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
 
 
 
@@ -32,11 +33,14 @@ int main()
     gpio_pull_up(I2C_SCL);
     // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
-    // Example to turn on the Pico W LED
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    // Example to blink the Pico W LED and print to serial monitor
 
     while (true) {
         printf("Hello, world!\n");
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(1000);
+        
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
         sleep_ms(1000);
     }
 }
