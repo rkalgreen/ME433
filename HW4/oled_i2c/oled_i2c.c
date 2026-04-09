06c734992fcec09cf94b5fb7709c712e1fc055c8
rkalgreen <ryan.kalgreen@gmail.com>
2026-04-09 11:46:16 -0500

    i2c pin changes to match my wiring

--- HW4/oled_i2c/oled_i2c.c
+++ HW4/oled_i2c/oled_i2c.c
@@ -6,9 +6,9 @@
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
 
 
 
