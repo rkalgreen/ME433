b1b515921b9bcda35697ede5bc320b16a533661a
rkalgreen <ryan.kalgreen@gmail.com>
2026-04-09 17:32:09 -0500

    fix .h file

--- HW4/oled_i2c/ssd1306.h
+++ HW4/oled_i2c/ssd1306.h
@@ -27,6 +27,7 @@ void ssd1306_setup(void);
 void ssd1306_update(void);
 void ssd1306_clear(void);
 void ssd1306_drawPixel(unsigned char x, unsigned char y, unsigned char color);
+void ssd1306_drawChar(unsigned char x, unsigned char y, char c);
 
 /// this should be private
 void ssd1306_command(unsigned char c);
