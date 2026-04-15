4255180c854ce0a6a9aab651995129752d7ac550
rkalgreen <ryan.kalgreen@gmail.com>
2026-04-14 21:55:19 -0500

    setting up libraries and helper functions

--- HW5/IMU_i2c/IMU_i2c.c
+++ HW5/IMU_i2c/IMU_i2c.c
@@ -2,15 +2,28 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "pico/cyw43_arch.h"
+#include "ssd1306.h"
+#include "font.h"
 
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
 
+// General read/write functions for I2C
+void setPin(char address, char reg, char value) {
+    char data[2] = {reg, value};
+    i2c_write_blocking(I2C_PORT, address, data, 2, false);
+}
 
+char readPin(char address, char reg) {
+    char value;
+    i2c_write_blocking(I2C_PORT, address, &reg, 1, true);  // true to keep host control of bus
+    i2c_read_blocking(I2C_PORT, address, &value, 1, false);  // false - finished with bus
+    return value;
+}
 
 
 int main()
@@ -32,11 +45,21 @@ int main()
     gpio_pull_up(I2C_SCL);
     // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
-    // Example to turn on the Pico W LED
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    // Set up the OLED display
+    ssd1306_setup();
+    ssd1306_clear();
+    ssd1306_update();
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        // onboard heartbeat LED
+        static int led_state = 0;
+        static int heartbeat_counter = 0;
+        heartbeat_counter++;
+        if (heartbeat_counter >= 10) {
+            led_state = !led_state;
+            cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_state);
+            heartbeat_counter = 0;
+        }
+        sleep_ms(100);
     }
 }
